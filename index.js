646affe6157be25086c621465e42a1c536fa5dee
everdrone <giorgiotropiano@gmail.com>
2018-12-19 03:14:18 +0100

    Add working code to index.js

--- index.js
+++ index.js
@@ -1,6 +1,57 @@
+import React, { Component } from 'react';
+import { requireNativeComponent, processColor } from 'react-native';
 
-import { NativeModules } from 'react-native';
+const WrappedShadowStackView = requireNativeComponent(
+  'ShadowStackView',
+  ShadowStackView
+);
 
-const { RNShadowStackView } = NativeModules;
+export default class ShadowStackView extends Component {
+  render() {
+    let { shadows, style, ...rest } = this.props;
+    let processShadows = shadows.slice();
+    for (let i in shadows) {
+      processShadows[i].color = processColor(shadows[i].color);
+    }
+    // RCTConvert workaround
+    let newShadows = {
+      ss: processShadows,
+    };
 
-export default RNShadowStackView;
+    let wrappedStyle = {};
+
+    if (style && style.backgroundColor) {
+      wrappedStyle.bckColor = processColor(style.backgroundColor);
+    }
+
+    const {
+      borderRadius,
+      borderTopLeftRadius,
+      borderTopStartRadius,
+      borderTopRightRadius,
+      borderTopEndRadius,
+      borderBottomLeftRadius,
+      borderBottomStartRadius,
+      borderBottomRightRadius,
+      borderBottomEndRadius,
+
+      backgroundColor,
+      shadowOpacity,
+      shadowRadius,
+      shadowColor,
+      shadowOffset,
+      borderColor,
+      borderWidth,
+      ...reducedStyle
+    } = style;
+
+    return (
+      <WrappedShadowStackView
+        {...rest}
+        shadows={newShadows}
+        {...reducedStyle}
+        {...wrappedStyle}
+      />
+    );
+  }
+}
